8e86a9b65740a265dd1def51b9ec9ea03fe6f045
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-10-30 03:48:00 +0200

    clerk auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <h1 className="h1-bold">Next 14</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Next 14</h1>
+      <UserButton afterSignOutUrl="/" />
+    </>
+  );
 }
