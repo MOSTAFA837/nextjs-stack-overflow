34cb3bb42dbb2206305a396d5993b32f6c8ff5cf
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-11-27 01:31:05 +0200

    ask a question frontend

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,13 @@
+import Question from "@/components/forms/Question";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+}
