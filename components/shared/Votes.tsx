b10fb950f44259d7ec67b1e777d80a03661469b6
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-01-07 01:35:56 +0200

    views system fix

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import { downvoteAnswer, upvoteAnswer } from "@/actions/answer.action";
 import { viewQuestion } from "@/actions/interaction";
